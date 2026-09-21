73f03853d542f94ec47b712c6a6feda140fd8aaa
차정호 <wjdgh0335@gmail.com>
2026-09-21 15:38:40 +0900

    CH02

---
+++ CH02/CH02Q02/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("파일: \"notes.txt\"");
+Console.WriteLine("경로: C:\\Study\\notes.txt\n");
+
+Console.WriteLine("저장이 완료되었습니다.");
+Console.WriteLine("파일을 열어 내용을 확인하세요.");
---
+++ CH02/CH02Q03/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.WriteLine("=== 문구점 영수증 ===\n");
+
+Console.WriteLine("공책: 1200원 x 3권 = 3600원");
+Console.WriteLine("펜: 800원 x 2권 = 1600원\n");
+
+Console.WriteLine($"합계: {1200 * 3 + 800 * 2}원");
+Console.WriteLine($"결제: 10000원");
+Console.WriteLine($"거스름돈: {10000 - 5200}원");

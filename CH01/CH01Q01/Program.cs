205b5863b9817de8782a03b60d31e557da1e209a
차정호 <wjdgh0335@gmail.com>
2026-09-21 13:18:59 +0900

    CH01 실습

---
+++ CH01/CH01Q01/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("안녕하세요, C#!");
+Console.WriteLine("Hello, C#!");
+Console.WriteLine("12345");
+Console.WriteLine("3 + 4 = 7");
+Console.WriteLine("10 / 2 = 5");
+Console.WriteLine("Console.WriteLine");
+Console.WriteLine("[시작] → [완료]");
---
+++ CH01/CH01Q02/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.WriteLine("   *");
+Console.WriteLine("  ***");
+Console.WriteLine(" *****");
+Console.WriteLine("*******");
+Console.WriteLine("  | |");
+Console.WriteLine("  |_|");

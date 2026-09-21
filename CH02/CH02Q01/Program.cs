547d312267bbfd2837323669a0766788ff29fb41
차정호 <wjdgh0335@gmail.com>
2026-09-21 15:43:53 +0900

    CH02

--- CH02/CH02Q01/Program.cs
+++ CH02/CH02Q01/Program.cs
@@ -4,7 +4,7 @@ using System.Security.Authentication;
 Console.WriteLine("18 + 3 = 21");
 Console.WriteLine("18 - 3 = 15");
 Console.WriteLine("18 * 3 = 54");
-Console.WriteLine("18 / 3 = 6");
+Console.WriteLine("18 / 3 = 6\n");
 
 Console.WriteLine("문자열 연결:"+" 18 + 3 = 21" );
 Console.WriteLine($"문자열 보간: {18} + {3} = {21}");

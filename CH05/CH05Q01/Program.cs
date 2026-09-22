932aad67f2e7be18c0e61eebdfcb24721bc7bd9c
차정호 <wjdgh0335@gmail.com>
2026-09-22 17:42:49 +0900

    CH05

---
+++ CH05/CH05Q01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("구역 순번: ");
+int Zone_num = int.Parse(Console.ReadLine());
+Console.Write("좌석 번호: ");
+int seat_num = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+Console.WriteLine($"좌석: ");
---
+++ CH05/CH05Q02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("1번: ");
+int frist= int.Parse(Console.ReadLine());
+Console.Write("2번: ");
+int second = int.Parse(Console.ReadLine());
+
+
+
+
+
+Console.WriteLine($"원문: ");
+Console.WriteLine($"같은 표기: ");
+Console.WriteLine($"같은 번호: ");

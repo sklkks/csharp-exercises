46c958706db9a2ddf11130c81697fe75ce59e623
차정호 <wjdgh0335@gmail.com>
2026-09-23 17:40:40 +0900

    CH05

--- CH05/CH05Q07/Program.cs
+++ CH05/CH05Q07/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("십의 자리: ");
+int ten = int.Parse(Console.ReadLine());
+Console.Write("일의 자리: ");
+int one = int.Parse(Console.ReadLine());
+
+char num = (char)(ten + '0');
+char num2 = (char)(one + '0');
+
+int num_int = ten * 10 + one;
+
+Console.WriteLine($"번호 표기: {num}{num2}");
+Console.WriteLine($"정수 값: {num_int}");

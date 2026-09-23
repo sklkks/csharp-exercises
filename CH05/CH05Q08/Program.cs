3c0197ebc25df13e3e5f00755c2f6f0eff29a781
차정호 <wjdgh0335@gmail.com>
2026-09-23 13:15:53 +0900

    CH05

---
+++ CH05/CH05Q08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("전체 금액: ");
+var totalprice = decimal.Parse(Console.ReadLine());
+
+Console.Write("인원수: ");
+var p = double.Parse(Console.ReadLine());
+
+int one = (int)totalprice / (int)p;
+int total = (int)p * one;
+decimal nam = totalprice - total;
+
+Console.WriteLine($"1인당 지급: {one}");
+Console.WriteLine($"총 지급: {total}");
+Console.WriteLine($"남은 금액: {nam}");
